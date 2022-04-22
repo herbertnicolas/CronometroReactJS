1e7fc01e5cf9c8bbdcfee1205d785575c5798614
herbertnicolas <hna@cin.ufpe.br>
2022-04-22 14:36:16 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
-
+import './styles.css'
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      tempo: 0,
+      estado: "GO"      
+    };
+    this.timer = null;
+    this.start = this.start.bind(this);
+    this.zerar = this.zerar.bind(this);
+  }
+
+  start(){
+    let estado = this.state.estado;
+    if(this.timer !== null){
+      clearInterval(this.timer);   //pausando timer
+      this.timer = null;
+      this.setState({estado: "GO"});
+    }else{
+      this.timer = setInterval(()=>{
+        let state = this.state;
+        state.tempo += 0.1;
+        this.setState(state);
+      },100)
+    this.setState({estado: "PAUSE"});
+    }
+    //console.log(this.timer);
+  }
+  zerar(){
+    let state = this.state;
+
+    if(state.tempo !== 0){
+      clearInterval(this.timer);
+      this.timer = null;
+      this.state.tempo = 0;
+      state.estado = "GO";
+    }
+    this.setState(state);
+  }
+
   render(){
     return(
-      <div>
-        <p>123</p>
-      </div>
+      <div className='container'>
+        <img src={require('./assets/cronometro.png')}/>
+        <p className='timer'>{this.state.tempo.toFixed(1)}</p>
+        <div className='areaBtn'>
+          <a className='botao' onClick={this.start}>{this.state.estado}</a>
+          <a className='botao' onClick={this.zerar}>CLEAR</a>
+        </div>
+      </div>  
     )
   }   
 }
